79e4c64fbe36803abbc72bd106b2edc5cfde2ef8
jose miguel <miguel.gamazo@galiclick.com>
2012-12-20 17:38:42 +0100

    Mas mejoras

--- app/models/spree/tire_green_tax.rb
+++
@@ -1,5 +0,0 @@
-class Spree::TireGreenTax < ActiveRecord::Base
-  attr_accessible :amount, :cat, :currency, :description, :weight_min, :weight_max
-
-  has_many :variants, :class_name => "spree_variants"
-end
