3f916c3bc082b3077c26c8773eb5db3a920a79ee
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-12-20 13:24:05 +0100

    Initial commit

--- app/models/spree/tire_green_tax.rb
+++ app/models/spree/tire_green_tax.rb
@@ -1,4 +1,4 @@
-class TireGreenTax < ActiveRecord::Base
+class Spree::TireGreenTax < ActiveRecord::Base
   attr_accessible :amount, :cat, :currency, :description, :weight_min, :weight_max
 
   has_many :variants, :class_name => "spree_variants"
