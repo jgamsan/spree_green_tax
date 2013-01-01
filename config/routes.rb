faf36282c9fcd8e1abea50af583201d30d8ff281
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2013-01-01 10:19:40 +0100

    Modificaciones en el Calculador

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Spree::Core::Engine.routes.draw do
   # Add your extension routes here
   namespace :admin do
     resources :tire_green_rates
+    resources :products do
+      member do
+        get 'tires_info'
+      end
+    end
   end
 end
