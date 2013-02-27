a8dc9bc63aad38d858562d223b333a55fc279e42
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2013-02-27 17:31:54 +0100

    Mas mejoras

--- app/models/spree/tire_green_rate.rb
+++ app/models/spree/tire_green_rate.rb
@@ -1,5 +1,5 @@
 class Spree::TireGreenRate < ActiveRecord::Base
   attr_accessible :amount, :cat, :currency, :description, :weight_min, :weight_max
 
-  has_many :variants, :class_name => "spree_variants"
+  has_many :variants, :class_name => "Spree::Variant"
 end
